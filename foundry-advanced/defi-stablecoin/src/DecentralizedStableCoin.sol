fe2cc45692413d95cf48c56b4ecaf36c2305a3d0
mahz24 <marcohurtado358@gmail.com>
2026-07-15 12:36:31 -0500

    feat: my first stablecoin statrted

---
+++ foundry-advanced/defi-stablecoin/src/DecentralizedStableCoin.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+/**
+ * @title DecentralizedStableCoin
+ * @author Marco Hurtado
+ * Collateral: Exogenous assets (e.g., ETH, BTC, etc.)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to a currency (e.g., USD, EUR, etc.)
+ * 
+ * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
+ */
+contract DecentralizedStableCoin {
+    constructor() {
+        // Constructor logic here
+    }
+}
+
